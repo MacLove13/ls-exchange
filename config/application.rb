e39b48d0181af32375ae9d4bf37721ef8617e4a5
MacLove013 <felipe.mourabarbosa@hotmail.com>
2019-08-12 16:38:39 -0300

    Adjust translate

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module LsExchange
 
  	config.time_zone = 'Brasilia'
 
+ 	config.i18n.default_locale = 'pt-BR'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
