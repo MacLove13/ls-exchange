06d19cf24ee076e3949b2fc86a3acf673df01619
MacLove013 <felipe.mourabarbosa@hotmail.com>
2019-08-12 00:15:47 -0300

    not change

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,7 +19,8 @@
 
 # Learn more: http://github.com/javan/whenever
 
-set :environment, "production"
+# set :environment, "production"
+set :environment, "development"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
 every 1.minute do
