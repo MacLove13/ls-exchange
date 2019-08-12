6e889046610301af3f6bf15b47796602af0a926b
MacLove013 <felipe.mourabarbosa@hotmail.com>
2019-08-11 22:52:38 -0300

    Add new camps to business

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_224058) do
+ActiveRecord::Schema.define(version: 2019_08_12_015138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 2019_08_10_224058) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "initials"
+    t.string "slogan"
+    t.integer "available_quotes"
+    t.integer "purchased_quotes"
   end
 
   create_table "quotes", force: :cascade do |t|
