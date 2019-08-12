4b4c0223fe8802854b6b3dfe63216f1a79158b4d
MacLove013 <felipe.mourabarbosa@hotmail.com>
2019-08-12 01:12:13 -0300

    Fix migrate

--- db/migrate/20190812040311_add_new_camps_to_users.rb
+++ db/migrate/20190812040311_add_new_camps_to_users.rb
@@ -1,6 +1,6 @@
 class AddNewCampsToUsers < ActiveRecord::Migration[5.2]
   def change
-  	add_column :users, :money, :float
+  	#add_column :users, :money, :float
   	add_column :users, :blocked_money, :float
   	add_column :users, :name, :string
   	add_column :users, :admin, :integer
