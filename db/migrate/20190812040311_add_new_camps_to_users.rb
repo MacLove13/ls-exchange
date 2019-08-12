0bf312c40b9ce86c22a698cad6b66a745e1e0b1c
MacLove013 <felipe.mourabarbosa@hotmail.com>
2019-08-12 01:05:27 -0300

    Adjust users table

---
+++ db/migrate/20190812040311_add_new_camps_to_users.rb
@@ -0,0 +1,8 @@
+class AddNewCampsToUsers < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :users, :money, :float
+  	add_column :users, :blocked_money, :float
+  	add_column :users, :name, :string
+  	add_column :users, :admin, :integer
+  end
+end
