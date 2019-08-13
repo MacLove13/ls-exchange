1ade224c831dd7d300c974317f32fd775f6d6eb4
Freeze <felipe.habbo.12@gmail.com>
2019-08-13 01:03:03 -0300

    add round

--- app/helpers/wallet_helper.rb
+++ app/helpers/wallet_helper.rb
@@ -1,8 +1,8 @@
 module WalletHelper
 	def calculate_gain(quantity, purchased_value, current_value)
 		
-    total_spend = purchased_value * quantity
-    actual_value = current_value * quantity
+    total_spend = (purchased_value * quantity).round(2)
+    actual_value = (current_value * quantity).round(2)
 
     @price = (actual_value - total_spend).round(2)
 	end
