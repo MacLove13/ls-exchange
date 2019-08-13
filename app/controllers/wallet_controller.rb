cc42e94b47fbf1d3ad18c61792b855309b6dd75f
Freeze <felipe.habbo.12@gmail.com>
2019-08-13 00:52:07 -0300

    Fix wallet page

--- app/controllers/wallet_controller.rb
+++ app/controllers/wallet_controller.rb
@@ -2,6 +2,6 @@ class WalletController < ApplicationController
   def index
   	return @error = 'dont_logged' if !user_signed_in?
 
-  	@quotes = Quote.where(owner_id: current_user.id).order('name')
+  	@quotes = Quote.where(owner_id: current_user.id)
   end
 end
