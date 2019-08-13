8c78f909f38d17780f0313bbf462faf9da317f99
Freeze <felipe.habbo.12@gmail.com>
2019-08-13 00:16:38 -0300

    Fix waller caller quote

--- app/controllers/wallet_controller.rb
+++ app/controllers/wallet_controller.rb
@@ -2,6 +2,6 @@ class WalletController < ApplicationController
   def index
   	return @error = 'dont_logged' if !user_signed_in?
 
-  	@quotes = Quotes.find_by(owner_id: current_user.id)
+  	@quotes = Quote.find_by(owner_id: current_user.id)
   end
 end
