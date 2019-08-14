b350e66cbf839e5394399558c300b81d3b68dbad
MacLove013 <felipe.mourabarbosa@hotmail.com>
2019-08-14 01:21:57 -0300

    Fix

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,9 +2,9 @@ class QuotesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def buy_quote
-  	@business = Business.find(params[:id])
+  	business = Business.find(params[:id])
 
-  	@buy_quantity = params[:buy_quantity].to_i
+  	buy_quantity = params[:buy_quantity].to_i
   	buy_value = params[:buy_value].to_f
 
     return @error = 'dont_logged' if !user_signed_in?
@@ -14,7 +14,7 @@ class QuotesController < ApplicationController
 
     return @error = "buy_value" if buy_value != historyValue.value
 
-    @total_price = buy_quantity * historyValue.value
+    total_price = buy_quantity * historyValue.value
 
     return @error = 'not_have_money' if current_user.money < total_price
 
@@ -33,7 +33,9 @@ class QuotesController < ApplicationController
 
     business.update_attributes(available_quotes: avail_quotes, purchased_quotes: purch_quotes)
 
-    @error = "Você comprou #{buy_quantity} ações da #{business.name}"
+    @business_name = business.name
+    @total_price = total_price
+    @buy_quantity = buy_quantity
 
     Rails.logger.info("#{current_user.name} comprou #{buy_quantity} ações da #{business.name} (#{business.id}) por $#{buy_value} cada. Total: #{total_price}")
   end
