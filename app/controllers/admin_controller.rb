05fb068adcd4e9649fce21ceff0d8720a3734b82
MacLove013 <felipe.mourabarbosa@hotmail.com>
2019-08-14 20:13:42 -0300

    Add dashboard admin

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,38 @@
+class AdminController < ApplicationController
+  def dashboard
+  	if !user_signed_in? || !current_user.admin?
+
+  		redirect_to root_path
+  		return
+  	end 
+
+  	lucro_total = 0
+  	@total_gain = 0
+
+  	@quotes = Quote.all
+
+  	@quotes.each do |quote|
+
+  		lucro_total = 0
+
+  		total_spend = (quote.purchased_value * quote.quantity)
+    	actual_value = (quote.current_value * quote.quantity)
+
+  		lucro_total = (actual_value - total_spend).round(2)
+
+  	end
+
+  	@total_gain = @total_gain + lucro_total
+
+  	@available_quotes = Business.all.sum('available_quotes')
+  	purch = Business.all.sum('purchased_quotes')
+  	total_quotes = @available_quotes + purch
+
+  	@total_quotes_all = (@quotes.count * 100) / total_quotes
+
+  	@percent = @total_quotes_all.to_s + '%'
+
+  	@wallet_total_money = User.all.sum('money')
+  	
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root to: 'landing_page#index'
   devise_for :users
 
+  # errors
+  get "/404", to: "errors#not_found"
+  get "/422", to: "errors#not_found"
+  get "/500", to: "errors#not_found"
+
   # business
   get 'business/:id', to: 'business#show', param: :id
   get 'all_business/:page', to: 'business#index', only: [:index], param: :page
@@ -12,11 +17,14 @@ Rails.application.routes.draw do
   get 'informations/index'
 
   # Admin Control
-  get '/admin/users', to: 'users#index'
-  get '/admin/find_user', to: 'users#index'
-  get '/admin/user/:id', to: 'users#show'
-  get '/admin/edit_user/:id', to: 'users#admin_edit', param: :id
-  post '/admin/update_user/:id' => 'users#update_user'
+  namespace :admin do
+    get 'dashboard'
+  end
+  get 'admin/users', to: 'users#index'
+  get 'admin/find_user', to: 'users#index'
+  get 'admin/user/:id', to: 'users#show'
+  get 'admin/edit_user/:id', to: 'users#admin_edit', param: :id
+  post 'admin/update_user/:id' => 'users#update_user'
   get 'admin/update_user/:id', to: redirect('/admin/users')
   get 'admin/wallet/:id', to: 'wallet#show'
 
