4786dad098542e79e5f8faa2c4fd22c703b2255d
MacLove013 <felipe.mourabarbosa@hotmail.com>
2019-08-15 03:37:13 -0300

    Adjust number currency to br

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+	def number_to_currency_br(number)
+	  number_to_currency(number, :unit => "R$ ", :separator => ",", :delimiter => ".")
+	end
+
   def is_admin?
     return false if current_user.admin.nil?
     current_user.admin > 0
