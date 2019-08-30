3cbe12a730716b27431b7715bccc1bda3175c89d
Freeze <felipe.habbo.12@gmail.com>
2019-08-29 21:26:36 -0300

    minor adjust

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -2,7 +2,7 @@ class BusinessController < ApplicationController
   skip_before_action :verify_authenticity_token
   
   def index
-  	@business = Business.where(bankrupt: nil).order(:name).page(params[:page])
+  	@business = Business.order(:name).page(params[:page])
 
   end
 
